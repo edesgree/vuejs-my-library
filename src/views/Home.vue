3da8deba8e5c01ebf5a601d0935a6b78bf66e892
Edouard Desgrée <edouard.desgree@gmail.com>
2023-09-03 00:22:23 +0200

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,32 +7,11 @@ import DummyText from '../components/DummyText.vue'
 const store = useMoviesStore()
 </script>
 <template>
-  <DummyText />
   <ul v-if="store.moviesResult" class="movies-list" :aria-busy="store.loading">
     <li v-for="movie in store.moviesResult" :key="movie.imdbID">
       <movie-card :movie="movie" type="search" />
     </li>
   </ul>
   <p>{{ store.searchError }}</p>
-  <div class="collapse bg-base-200">
-    <input type="radio" name="my-accordion-1" checked="checked" />
-    <div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
-    <div class="collapse-content">
-      <p>hello</p>
-    </div>
-  </div>
-  <div class="collapse bg-base-200">
-    <input type="radio" name="my-accordion-1" />
-    <div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
-    <div class="collapse-content">
-      <p>hello</p>
-    </div>
-  </div>
-  <div class="collapse bg-base-200">
-    <input type="radio" name="my-accordion-1" />
-    <div class="collapse-title text-xl font-medium">Click to open this one and close others</div>
-    <div class="collapse-content">
-      <p>hello</p>
-    </div>
-  </div>
+  <DummyText />
 </template>
