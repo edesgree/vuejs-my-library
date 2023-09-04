dc2820a47e460c96dc9435982e0855581903e12d
Edouard Desgrée <edouard.desgree@gmail.com>
2023-09-04 23:45:02 +0200

    add api credentials

--- src/api.js
+++ src/api.js
@@ -1,13 +1,35 @@
 import axios from 'axios'
 // fetch OMDB API with search query
-const apiKEY = 'ce739ee3'
+const OMDB_API_KEY = 'ce739ee3'
 const OMDB_API_URL = 'https://www.omdbapi.com/'
+
+export async function fetchMoviesTMDB(query) {
+  const url = `https://api.themoviedb.org/3/discover/movie?with_keywords=${query}`
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTJlZjc2ZGNkYjkwYjliY2FkYmM2YTlhNzgwYWIyOCIsInN1YiI6IjY0ZjY0YTI0NzdkMjNiMDEyZThjMDVjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uCpm9WJuz42jwhVfBGaBwP0K-q2PpUEZktlNksMMxzw'
+    }
+  }
+  try {
+    const res = axios.get(url, options)
+    const data = res.data
+    console.log('search result TMDB', data)
+    return data || []
+  } catch (err) {
+    console.error('Error fetching movies by query:', err)
+    throw new Error(err)
+  }
+}
 
 export async function fetchMovies(query) {
   try {
-    const response = await axios.get(`${OMDB_API_URL}?apikey=${apiKEY}&s=${query}`)
+    const response = await axios.get(`${OMDB_API_URL}?apikey=${OMDB_API_KEY}&s=${query}`)
     const data = response.data
-    console.log('search result', data)
+    console.log('search result OMDB', data)
+
     return data || []
   } catch (err) {
     console.error('Error fetching movies by query:', err)
@@ -20,7 +42,7 @@ export async function fetchMovies(query) {
 // fetch OMDB API with movie ID, that gets more details than the one above
 export async function fetchMovieById(id) {
   try {
-    const response = await axios.get(`${OMDB_API_URL}?apikey=${apiKEY}&i=${id}&plot=short`)
+    const response = await axios.get(`${OMDB_API_URL}?apikey=${OMDB_API_KEY}&i=${id}&plot=short`)
     const data = response.data
     return data || []
   } catch (err) {
