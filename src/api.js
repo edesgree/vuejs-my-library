de6cb328a58b0287ed5979afbb4d1b10a14c4725
Edouard Desgrée <edouard.desgree@gmail.com>
2023-09-05 07:39:31 +0200

    protect sensitive info in env variables

--- src/api.js
+++ src/api.js
@@ -1,29 +1,29 @@
 import axios from 'axios'
-// fetch OMDB API with search query
-const OMDB_API_KEY = 'ce739ee3'
-const OMDB_API_URL = 'https://www.omdbapi.com/'
-//const TMDB_API_KEY = '752ef76dcdb90b9bcadbc6a9a780ab28'
+
+// fetch TMDB API with search query
+// const TMDB_API_KEY = import.meta.env.TMDB_API_KEY
+const VITE_TMDB_SECRET_KEY = import.meta.env.VITE_TMDB_SECRET_KEY
 const TMDB_API_URL = 'https://api.themoviedb.org/3/'
 const TMDB_API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTJlZjc2ZGNkYjkwYjliY2FkYmM2YTlhNzgwYWIyOCIsInN1YiI6IjY0ZjY0YTI0NzdkMjNiMDEyZThjMDVjNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uCpm9WJuz42jwhVfBGaBwP0K-q2PpUEZktlNksMMxzw'
+    Authorization: `Bearer ${VITE_TMDB_SECRET_KEY}`
   }
 }
 export async function fetchMoviesTMDB(query) {
   try {
+    console.log('TMDB_SECRET_KEY:', import.meta.env.TMDB_SECRET_KEY)
     const response = await axios.get(
       `${TMDB_API_URL}search/movie?include_adult=true&query=${query}`,
       TMDB_API_OPTIONS
     )
     const data = response.data
     console.log('search result TMDB', data)
-    return data || []
-  } catch (err) {
-    console.error('Error fetching movies by query:', err)
-    throw new Error(err)
+    return data
+  } catch (error) {
+    console.error('Error fetching movies by query:', error)
+    throw error
   } finally {
     console.log('done fetching movies by query with TMDB')
   }
@@ -35,40 +35,11 @@ export async function fetchMovieByIdTMDB(id) {
     const response = await axios.get(`${TMDB_API_URL}movie/${id}`, TMDB_API_OPTIONS)
     const data = response.data
     console.log('(TMDB)  fetching movie by Id', data)
-    return data || []
-  } catch (err) {
-    console.error('(TMDB) Error fetching movie by Id:', err)
-    throw new Error(err)
+    return data
+  } catch (error) {
+    console.error('(TMDB) Error fetching movie by Id:', error)
+    throw error
   } finally {
     console.log('(TMDB) Done fetching movie by Id')
-  }
-}
-
-export async function fetchMoviesOMDB(query) {
-  try {
-    const response = await axios.get(`${OMDB_API_URL}?apikey=${OMDB_API_KEY}&s=${query}`)
-    const data = response.data
-    console.log('search result OMDB', data)
-    fetchMoviesTMDB(query)
-    return data || []
-  } catch (err) {
-    console.error('Error fetching movies by query:', err)
-    throw new Error(err)
-  } finally {
-    console.log('done fetching movies by query')
-  }
-}
-
-// fetch OMDB API with movie ID, that gets more details than the one above
-export async function fetchMovieByIdOMDB(id) {
-  try {
-    const response = await axios.get(`${OMDB_API_URL}?apikey=${OMDB_API_KEY}&i=${id}&plot=short`)
-    const data = response.data
-    return data || []
-  } catch (err) {
-    console.error('Error fetching movie by Id:', err)
-    throw new Error(err)
-  } finally {
-    console.log('done fetching movie by Id')
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dotenv from 'dotenv'
+
+//Load environment variable from .env file
+dotenv.config()
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
